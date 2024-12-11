a7ded3694131ab34b0ff4ef3791ac7bf639b7562
angelica <guidolinangelica66@gmail.com>
2024-12-11 18:40:55 +0100

    feat add result

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ const countDownElm = document.getElementById("countdown")
 const numbDiv= document.getElementById("numbers")
 const formNumbHid= document.getElementById("random-number-hidden")
 const randomNumbShowRes= document.getElementById("random-number-show-result")
+const checkNumb= document.getElementById("checkit")
 //VARIABLES
-let count = 5;
+let count = 15;
 const arrayRandomNumb = Array.from({ length: 5 }, () => Math.floor(Math.random() * 100));
 numbDiv.textContent = arrayRandomNumb.join(' - ');
 console.log(numbDiv)
@@ -17,5 +18,13 @@ const timer = setInterval(function() {
    
   }
   count--;
-
 }, 1000);
+function checkNumbers() {
+  const nums = Array.from(document.getElementById('numb')).map(input => Number(input.value));
+  const matchedNumbers = arrayRandomNumb.filter(num => nums.includes(num));
+  randomNumbShowRes = alert(`Hai indovinato ${matchedNumbers.length} numeri: ${matchedNumbers.join(', ')}`);
+  randomNumbShowRes.classList.remove('hidden');
+}
+
+  
+
